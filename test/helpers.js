eed8da02f358055cb0ce2afe97e99d3e91e07f0b
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-05-21 16:04:41 +0900

    add jest-styled-components

--- test/helpers.js
+++ test/helpers.js
@@ -1,12 +1,11 @@
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
+import 'jest-styled-components';
 
 configure({ adapter: new Adapter() });
 
-export const elementToJson = element =>
-  // shallowだとstyled-componentsが展開されない。mountだとstyled-components以外も展開されちゃうし重い。
-  toJson(shallow(element));
+export const elementToJson = element => toJson(shallow(element));
 
 export const snapshot = (title, element) =>
   it(`should match snapshot: ${title}`, () => {

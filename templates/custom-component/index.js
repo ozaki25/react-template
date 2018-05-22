101d4c4a46c113cc72c82a51253925d558592ff3
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-05-22 17:12:58 +0900

    update component template

--- templates/custom-component/index.js
+++ templates/custom-component/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const COMPONENT_NAME = () => (
-  <div>COMPONENT_NAME</div>
-);
+function COMPONENT_NAME({}) {
+  return <div>COMPONENT_NAME</div>;
+}
 
-COMPONENT_NAME.propTypes = {
+COMPONENT_NAME.propTypes = {};
 
-};
-
-COMPONENT_NAME.defaultProps = {
-
-};
+COMPONENT_NAME.defaultProps = {};
 
 export default COMPONENT_NAME;

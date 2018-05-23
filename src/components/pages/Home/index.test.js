ea9f0050e12df41cf0ce2af414aaa4e79a82dafd
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-05-23 13:43:42 +0900

    update snapshot page home

--- src/components/pages/Home/index.test.js
+++ src/components/pages/Home/index.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { snapshot } from '../../../../test/helpers';
 import Home from './';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>,
-    div,
-  );
-  ReactDOM.unmountComponentAtNode(div);
-});
+const props = {
+  greetings: [],
+  greet: jest.fn(),
+  bye: jest.fn(),
+};
+
+snapshot('Hello', <Home {...props} />);

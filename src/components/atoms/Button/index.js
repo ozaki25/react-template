f7e0d85395bd125928a4542e35ed1cf54bfd688c
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-05-24 17:54:43 +0900

    add reset button

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  &:active {
+    opacity: 0.5;
+  }
+`;

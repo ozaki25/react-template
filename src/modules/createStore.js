0b5fa9d376ee7e53f4d8d0adaff4c52b676b9a89
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-05-24 11:18:50 +0900

    add redux logger

--- src/modules/createStore.js
+++ src/modules/createStore.js
@@ -1,13 +1,21 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-/* eslint-disable no-underscore-dangle */
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
 const store = createStore(
   reducer,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  compose(
+    applyMiddleware(...middleware),
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
 );
-/* eslint-enable */
 
 export default store;

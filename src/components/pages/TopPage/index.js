77a89069ec11c9ccf5b50f9f03c6cf1dec1690a7
ozaki <fs65ugumori@yahoo.co.jp>
2018-06-05 08:31:38 +0900

    add loading test to TopPage

--- src/components/pages/TopPage/index.js
+++ src/components/pages/TopPage/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import NavigationBar from 'src/components/molecules/NavigationBar';
 import Main from 'src/components/organisms/TopPage/Main';
+import Loading from 'src/components/atoms/loading/Loading';
 
-const TopPage = () => (
-  <React.Fragment>
-    <NavigationBar title="アプリメニュー" />
-    <Main onClickStartReissue={() => console.log('click')} />
-  </React.Fragment>
-);
+class TopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false };
+  }
+
+  componentDidMount() {
+    // Loading test
+    setInterval(() => this.setState({ loading: !this.state.loading }), 3000);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar title="アプリメニュー" />
+        <Main onClickStartReissue={() => console.log('click')} />
+        <Loading show={this.state.loading} />
+      </React.Fragment>
+    );
+  }
+}
 
 export default TopPage;

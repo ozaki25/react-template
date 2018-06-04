9ec820d0bf9ee4b3c3d89a34e3679f45b2c00c13
ozaki <fs65ugumori@yahoo.co.jp>
2018-06-05 07:33:25 +0900

    modify button component

--- src/components/atoms/buttons/MenuButton/index.js
+++ src/components/atoms/buttons/MenuButton/index.js
@@ -2,33 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-function MenuButton({ children, onClick }) {
-  return (
-    <Button onClick={onClick}>
-      <Label>{children}</Label>
-    </Button>
-  );
-}
+const MenuButton = ({ children, onClick }) => <Button onClick={onClick}>{children}</Button>;
 
 export const Button = styled.button`
   border: solid 1px #eee;
   box-shadow: 1px 1px 1px #bdbdbd;
+  color: #212121;
   display: inline-block;
+  font-size: 14px;
+  font-weight: bold;
   height: 44px;
   padding-left: 10px;
   padding-right: 10px;
+  text-align: left;
   width: 300px;
   &:active {
     opacity: 0.5;
     box-shadow: 1px 1px 1px #f5f5f5;
   }
-`;
-
-export const Label = styled.p`
-  color: #212121;
-  font-size: 14px;
-  font-weight: bold;
-  text-align: left;
 `;
 
 MenuButton.propTypes = {

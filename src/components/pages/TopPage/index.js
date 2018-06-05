9585434b7f94d34481bcc0efe59e4a0bec672f75
ozaki <fs65ugumori@yahoo.co.jp>
2018-06-06 07:42:09 +0900

    modify TopPage component

--- src/components/pages/TopPage/index.js
+++ src/components/pages/TopPage/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NavigationBar from 'src/components/molecules/NavigationBar';
 import Main from 'src/components/organisms/TopPage/Main';
 import Loading from 'src/components/atoms/loading/Loading';
 
 class TopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false };
-  }
-
   componentDidMount() {
-    // Loading test
-    setInterval(() => this.setState({ loading: !this.state.loading }), 3000);
+    this.props.createUserId();
   }
 
   render() {
@@ -19,10 +14,17 @@ class TopPage extends React.Component {
       <React.Fragment>
         <NavigationBar title="アプリメニュー" />
         <Main onClickStartReissue={() => console.log('click')} />
-        <Loading show={this.state.loading} />
+        <Loading show={this.props.loading} />
+        <p>{this.props.userId}</p>
       </React.Fragment>
     );
   }
 }
+
+TopPage.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  userId: PropTypes.string.isRequired,
+  createUserId: PropTypes.func.isRequired,
+};
 
 export default TopPage;
--- src/components/pages/TopPage/index.test.js
+++ src/components/pages/TopPage/index.test.js
@@ -2,4 +2,24 @@ import React from 'react';
 import { snapshot } from 'test/helpers';
 import TopPage from '.';
 
-snapshot('TopPage', <TopPage />);
+const props1 = {
+  loading: false,
+  userId: '',
+  createUserId: jest.fn(),
+};
+
+const props2 = {
+  loading: true,
+  userId: '',
+  createUserId: jest.fn(),
+};
+
+const props3 = {
+  loading: false,
+  userId: 'ABC123',
+  createUserId: jest.fn(),
+};
+
+snapshot('TopPage/ユーザID取得前', <TopPage {...props1} />);
+snapshot('TopPage/ユーザID取得中', <TopPage {...props2} />);
+snapshot('TopPage/ユーザID取得後', <TopPage {...props3} />);

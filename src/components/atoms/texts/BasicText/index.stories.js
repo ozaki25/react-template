b39f4b1719cf3bff85641fe6fe4e3bf3ab76dbcb
ozaki <fs65ugumori@yahoo.co.jp>
2018-06-13 18:29:35 +0900

    modify basic text component

--- src/components/atoms/texts/BasicText/index.stories.js
+++ src/components/atoms/texts/BasicText/index.stories.js
@@ -9,6 +9,29 @@ import BasicText from '.';
 const stories = storiesOf('atoms/texts', module);
 
 stories.add(
-  'BasicText',
+  'BasicText/nomal',
   withInfo('標準文字列')(() => <BasicText>{text('text', '標準文字列')}</BasicText>),
+);
+
+stories.add(
+  'BasicText/emphasis',
+  withInfo('強調する文字列')(() => (
+    <BasicText emphasis>{text('text', '強調する文字列')}</BasicText>
+  )),
+);
+
+stories.add(
+  'BasicText/red',
+  withInfo('強調する赤い文字列')(() => (
+    <BasicText red>{text('text', '強調する赤い文字列')}</BasicText>
+  )),
+);
+
+stories.add(
+  'BasicText/red-emphasis',
+  withInfo('強調する赤い文字列')(() => (
+    <BasicText emphasis red>
+      {text('text', '強調する赤い文字列')}
+    </BasicText>
+  )),
 );
--- src/components/atoms/texts/BasicText/index.test.js
+++ src/components/atoms/texts/BasicText/index.test.js
@@ -2,4 +2,12 @@ import React from 'react';
 import { snapshot } from 'test/helpers';
 import BasicText from '.';
 
-snapshot('BasicText', <BasicText>テキスト</BasicText>);
+snapshot('BasicText/nomal', <BasicText>テキスト</BasicText>);
+snapshot('BasicText/emphasis', <BasicText emphasis>テキスト</BasicText>);
+snapshot('BasicText/red', <BasicText red>テキスト</BasicText>);
+snapshot(
+  'BasicText/red-emphasis',
+  <BasicText red emphasis>
+    テキスト
+  </BasicText>,
+);

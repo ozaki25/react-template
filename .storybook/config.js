d5dfe3ba40a73b2d36b3d024d70129f2d75adc82
ozaki <fs65ugumori@yahoo.co.jp>
2018-06-19 08:00:33 +0900

    add storybook screenshot plugin

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,27 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { initScreenshot, withScreenshot } from 'storybook-chrome-screenshot';
 
 addDecorator(withKnobs);
+addDecorator(initScreenshot());
+addDecorator(
+  withScreenshot({
+    viewport: [
+      {
+        width: 320,
+        height: 480,
+        isMobile: true,
+        hasTouch: true,
+      },
+      {
+        width: 414,
+        height: 736,
+        isMobile: true,
+        hasTouch: true,
+      },
+    ],
+  }),
+);
 
 const req = require.context('src/components', true, /.stories.js$/);
 
